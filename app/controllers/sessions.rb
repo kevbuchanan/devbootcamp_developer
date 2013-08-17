6e44126f0b2d49df0272ece9ba805a39ef989d15
kevinbuch <kevaustinbuch@gmail.com>
2013-08-17 11:36:07 -0500

    created oauth helpers and refactored sessions controller

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,18 +1,14 @@
 enable :sessions
 
 get '/sign_in' do
-  redirect to oauth_client.auth_code.authorize_url(redirect_uri: 'http://localhost:9393/auth' )
-end
-
-get '/session/destroy' do
-  session.clear
-  redirect to('/')
+  redirect to dbc_auth
 end
 
 get '/auth' do
-  token = oauth_client.auth_code.get_token(params[:code], redirect_uri: 'http://localhost:9393/auth')
-  user = token.get('/me')
-  session[:user] = JSON.parse(user.body)
+  token = get_oauth_token(params[:code])
+  user_data = get_user(token)
+  user_attributes = JSON.parse(user_data.body)
+  session[:user_attributes] = user_attributes
   session[:oauth_token] = token_as_hash(token)
   redirect to ('/')
 end

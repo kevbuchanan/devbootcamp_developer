4d7c5c810b884869f29afdeb8b66c76bbde67cf1
Abraham Sangha <abraham.sangha@gmail.com>
2013-08-18 15:49:23 -0500

    added specs for sign_in method on sessions_controller
    The first test simply checks the response to hitting the sign in route, which should, firstly, be a redirect, and secondly, take the client to DBC's Auth service.
    
    The second and third tests ensure that the correct query is made to DBC Auth.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'uri'
 
-describe "/sign_in" do
+describe "sign_in" do
 	before do
 		get "/sign_in"
 		@uri = URI(last_response.location)
@@ -14,38 +14,20 @@ describe "/sign_in" do
 
 	context "checking querystring arguments" do
 		before do
-			@args  = Rack::Utils.parse_query @uri.query
+			@args  = Rack::Utils.parse_query(@uri.query)
 		end
 
 		it "should request an authorization code" do
 			@args["response_type"].should eq "code"
 		end
 
-		it "wants the user sent back to the /auth endpoint" do
+		it "should include the correct redirect_uri" do
 			redirect_uri = URI(@args["redirect_uri"])
 			redirect_uri.path.should eq "/auth"
 		end
+
+		it "should include the client id" do
+			@args["client_id"].should_not be_nil
+		end
 	end
 end
-
-# describe "/auth" do
-# 	it "does stuff" do
-# 		visit "/sign_in"
-# 		# within(".string email required") do
-# 			fill_in "Email", :with => "tanner@devbootcamp.com"
-# 			fill_in "Password", :with => "pizza"
-# 		# end
-# 		click_button('Authenticate')
-# 		save_and_open_page
-# 		expect(page).to have_content "Hello"
-# 	end
-# end
-
-# describe "/sign_out" do
-# 	it "clears the session and redirects to the index" do
-# 		session = mock(:session)
-# 		session.stub(:[]).with(:user_id).and_return(1)
-# 		visit "/sign_out"
-# 		expect ( session[:user_id] = nil)
-# 		current_path.should == "/"
-# 	end

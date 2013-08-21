04263567ea290cae020fd95d79a014099f9c3fb7
kevinbuch <kevaustinbuch@gmail.com>
2013-08-21 16:26:03 -0500

    removed unnecessary methods

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -8,11 +8,9 @@ get '/sign_in' do
   redirect to ('/auth/dbc')
 end
 
-get '/auth/:provider/callback' do
-  user_attributes = request.env['omniauth.auth'].info
-  session[:user_attributes] = user_attributes
-  token = request.env['omniauth.auth'].credentials
-  session[:oauth_token] = token_as_hash(token)
+get '/auth/dbc/callback' do
+  session[:user_attributes] = request.env['omniauth.auth'].info
+  session[:oauth_token] = token_as_hash(request.env['omniauth.auth'].credentials)
   redirect to ('/')
 end
 

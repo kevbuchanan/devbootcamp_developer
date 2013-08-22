9db352beefc3a584a7afd397a4093ecb87c62d8f
William Karsten <yme_ynot45@hotmail.com>
2013-08-22 12:44:30 -0500

    test workaround

---
+++ spec/requests/integration_helper_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'uri'
+
+describe "IntegrationHelper" do
+
+  context "contact email form" do
+    before do
+      get "/contact"
+    end
+
+    it "The form loads correctly when not logged in" do
+      page.should have_content("SEND")
+    end
+
+    it "clicking SEND redirects you to documentation" do
+      click_button "SEND"
+      current_path.should eq "/documentation"
+    end
+  end
+
+  it "should send a contact email" do
+    do_not_send_email
+    Pony.should_receive(:deliver) do |mail|
+        mail.to.should == [ 'development@devbootcamp.com' ]
+    end
+  end
+end
